9d8d4cc90fa1f08c8ecde16fb2e3e0c93e0a28d7
root <root@localhost.localdomain>
2018-01-14 14:42:45 +0800

    new beta

---
+++ blog/__init__.py
@@ -0,0 +1,4 @@
+from .wsgi import create_app
+
+app = create_app()
+
--- blog/models.py
+++ blog/models.py
@@ -12,14 +12,14 @@ category_entries = db.table('category_entries',
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
-    description = db.Ccolumn(db.text)
-    is_visible = db.column(db.Boolean, default=True)
+    description = db.Column(db.Text(1000))
+    is_visible = db.column(db.Boolean)
 
 
 class Entries(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(256), nullable=False)
-    content = db.Column(db.text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
     categorys = db.relationship('Category', secondary=category_entries,
                                 backref=db.backref('entries', lazy='dynamic'))
 
