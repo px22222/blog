8cfd76f027f2797a4f6ce6ac636f5e964d1e8df9
root <root@localhost.localdomain>
2018-02-27 18:33:40 +0800

    fix some issue

---
+++ blog/jinja2/custom_filter.py
@@ -0,0 +1,13 @@
+
+
+
+def clipping_content(content, length=200):
+    print (type(content))
+    print (content)
+    fixed_length_content = content
+    # if len(content) > length:
+    #     fixed_length_content  =  content[:length] + '...'
+    # print (fixed_length_content)
+    return  fixed_length_content
+
+
